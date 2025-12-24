9aa8beb3bccac10aa9f1ccc85e016f8d30fd761b
Gabriel Valerio <gabrielvalerio2929@gmail.com>
2025-12-23 23:46:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+class MyBot(commands.Bot):
+    async def setup_hook(self):
+        await self.load_extension("music")
+
+bot = MyBot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"✅ Bot online como {bot.user}")
-
-bot.load_extension("music")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
